4df084f082d912dbef8382cb708f1e4130d19255
Eren Findik <findike10@gmail.com>
2023-08-30 11:01:10 +0300

    Add project files.

---
+++ ApiConsume/HotelProject.DataAccessLayer/EntityFrameework/EfRoomDal.cs
@@ -0,0 +1,20 @@
+﻿using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concreate;
+using HotelProject.DataAccessLayer.Repositories;
+using HotelProject.EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.EntityFrameework
+{
+    public class EfRoomDal : GenericRepository<Room>, IRoomDal
+    {
+        public EfRoomDal(Context context) : base(context) 
+        {
+            
+        }
+    }
+}
---
+++ ApiConsume/HotelProject.DataAccessLayer/EntityFrameework/EfSubscribeDal.cs
@@ -0,0 +1,19 @@
+﻿using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concreate;
+using HotelProject.DataAccessLayer.Repositories;
+using HotelProject.EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.EntityFrameework
+{
+    public class EfSubscribeDal : GenericRepository<Subscribe>, ISubscribeDal
+    {
+        public EfSubscribeDal(Context context) : base(context)
+        {
+        }
+    }
+}
