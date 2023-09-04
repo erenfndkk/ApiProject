fea8193d88ce8035a486e7632c58d22b62e0560e
Eren Findik <findike10@gmail.com>
2023-09-04 20:38:04 +0300

    AppUser için CreateDto yapıldı

---
+++ Frontend/HotelProject.WebUI/Dtos/RegisterDto/CreateNewUserDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.RegisterDto
+{
+    public class CreateNewUserDto
+    {
+        [Required(ErrorMessage = "Lütfen isim giriniz")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Lütfen soyad giriniz")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Lütfen kullanıcı adı giriniz")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Lütfen mail giriniz")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifre giriniz")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifre tekrar giriniz")]
+        [Compare("Password", ErrorMessage ="Şifreler uyuşmuyor")]
+        public string ConfirmPassword { get; set; }
+    }
+}
