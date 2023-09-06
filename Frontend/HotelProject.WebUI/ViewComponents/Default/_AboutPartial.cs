64b650ccf5aa1dae54d3569165826f3f86ce6218
Eren Findik <findike10@gmail.com>
2023-09-06 15:58:59 +0300

    sıfırdan About nesnesi varlığı oluşturuldu miramaride tüm işlemleri yapıldı

--- Frontend/HotelProject.WebUI/ViewComponents/Default/_AboutPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_AboutPartial.cs
@@ -1,11 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.AboutDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _AboutPartial : ViewComponent
     {
+        //private readonly IHttpClientFactory _httpClientFactory;
+        //public _AboutPartial(IHttpClientFactory httpClientFactory)
+        //{
+        //    _httpClientFactory = httpClientFactory;
+        //}
         public IViewComponentResult Invoke()
         {
+            //var client = _httpClientFactory.CreateClient();
+            //var responseMessage = await client.GetAsync("http://localhost:5103/api/About");
+            //if (responseMessage.IsSuccessStatusCode)
+            //{
+            //    var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            //    var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+            //    return View(values);
+            //}
             return View();
         }
     }
