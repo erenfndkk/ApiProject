0c2358e48e797734a5abc4332a61d5e539adb6b9
Eren Findik <findike10@gmail.com>
2023-09-08 12:24:43 +0300

    Trailer, service, testimonial ve staff dinamik hale getirildi.

--- Frontend/HotelProject.WebUI/Dtos/StaffDto/ResultStaffDto.cs
+++ Frontend/HotelProject.WebUI/Dtos/StaffDto/ResultStaffDto.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HotelProject.EntityLayer.Concreate
+﻿namespace HotelProject.WebUI.Dtos.StaffDto
 {
-    public class Staff
+    public class ResultStaffDto
     {
-        public int StaffID { get; set; }
         public string Name { get; set; }
         public string Title { get; set; }
         public string SocialMedia1 { get; set; }
