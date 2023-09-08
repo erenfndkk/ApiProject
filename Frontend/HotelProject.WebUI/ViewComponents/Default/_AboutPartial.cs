7cb372cf43113ff6c9cab54f5af6ce08567d14e0
Eren Findik <findike10@gmail.com>
2023-09-08 10:56:59 +0300

    Room için işlemler tamamlandı

--- Frontend/HotelProject.WebUI/ViewComponents/Default/_AboutPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_AboutPartial.cs
@@ -6,21 +6,21 @@ namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _AboutPartial : ViewComponent
     {
-        //private readonly IHttpClientFactory _httpClientFactory;
-        //public _AboutPartial(IHttpClientFactory httpClientFactory)
-        //{
-        //    _httpClientFactory = httpClientFactory;
-        //}
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _AboutPartial(IHttpClientFactory httpClientFactory)
         {
-            //var client = _httpClientFactory.CreateClient();
-            //var responseMessage = await client.GetAsync("http://localhost:5103/api/About");
-            //if (responseMessage.IsSuccessStatusCode)
-            //{
-            //    var jsonData = await responseMessage.Content.ReadAsStringAsync();
-            //    var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
-            //    return View(values);
-            //}
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5103/api/About");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
